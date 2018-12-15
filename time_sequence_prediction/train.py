ed75d81f082f0e3d7b90e7cdd9c833bb4f22e46f
s-kypr <sofia.kyp273@gmail.com>
2018-12-15 20:14:57 +0100

    added embedding in train.py

--- time_sequence_prediction/train.py
+++ time_sequence_prediction/train.py
@@ -4,13 +4,19 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from tensorboardX import SummaryWriter
+import json
+
+VOCAB_SIZE = 282
+EMBED_SIZE = 7
+EPOCHS = 500
 
 class Sequence(nn.Module):
     def __init__(self):
         super(Sequence, self).__init__()
-        self.lstm1 = nn.LSTMCell(1, 51)
+        self.embed = torch.nn.Embedding(VOCAB_SIZE, EMBED_SIZE) # 2 words in vocab, 5 dimensional embeddings
+        self.lstm1 = nn.LSTMCell(EMBED_SIZE, 51)
         self.lstm2 = nn.LSTMCell(51, 51)
-        self.linear = nn.Linear(51, 1129)
+        self.linear = nn.Linear(51, VOCAB_SIZE) # make it with our vocabulary size
 
     def forward(self, input, future = 0):
         outputs = []
@@ -20,12 +26,18 @@ class Sequence(nn.Module):
         c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
 
         for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            x_embedded = self.embed(input_t.view(-1).long())
+
+            h_t, c_t = self.lstm1(x_embedded, (h_t, c_t))
             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
             output = self.linear(h_t2)
+
             outputs += [output]
         for i in range(future):# if we should predict the future
             output = torch.argmax(output, dim=1).view(-1, 1).double()
+
+            output = self.embed(output.view(-1).long())
+
             h_t, c_t = self.lstm1(output, (h_t, c_t))
             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
             output = self.linear(h_t2)
@@ -33,6 +45,16 @@ class Sequence(nn.Module):
         outputs = torch.stack(outputs, 1).squeeze(2)
         return outputs
 
+    def show_embedding(self, writer):
+
+        with open('../data/mappings.json', "r") as infile:
+            mapping = json.load(infile)
+
+        input = torch.FloatTensor(list(mapping['chord'].values()))
+        labels = list(mapping['chord'].keys())
+
+
+        writer.add_embedding(self.embed(input.long()), metadata=labels)
 
 if __name__ == '__main__':
     # set random seed to 0
@@ -47,7 +69,7 @@ if __name__ == '__main__':
     test_target = torch.from_numpy(data[:3, 1:])
 
     # tensorboard
-    writer = SummaryWriter(log_dir='../results/tboard/')
+    writer = SummaryWriter(log_dir='../results/tboard/embeddings')
 
     # build the model
     seq = Sequence()
@@ -57,8 +79,9 @@ if __name__ == '__main__':
     # use LBFGS as optimizer since we can load the whole data to train
     optimizer = optim.Adam(seq.parameters(), lr=0.01)
     #begin to train
-    for i in range(500):
+    for i in range(EPOCHS):
         print('STEP: ', i)
+
         def closure():
             optimizer.zero_grad()
             out = seq(input)
@@ -80,4 +103,5 @@ if __name__ == '__main__':
             print('test loss:', loss.item())
             y = pred.detach().numpy()
 
+    seq.show_embedding(writer)
     writer.close()
