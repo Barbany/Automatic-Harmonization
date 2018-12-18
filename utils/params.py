df463fe9ffe6d54bd7e835dd05996c495eb4baa9
Barbany <oriol.barbany@alu-etsetb.upc.edu>
2018-12-18 10:59:26 +0100

    Conflict params

--- utils/params.py
+++ utils/params.py
@@ -53,3 +53,41 @@ def parse_arguments():
     parser.set_defaults(**default_params)
 
     return parser.parse_args()
+
+
+def parse_arguments_prediction():
+    """
+    Set arguments from the command line when running 'run.py'. Run with option '-h' or '--help' for
+    information about parameters and its usage.
+    :return: parser.parse_args()
+    """
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        argument_default=argparse.SUPPRESS
+    )
+    parser.add_argument(
+        '-V', '--verbose', dest='verbose', action='store_true',
+        help='Provide additional details about the program. This level of detail'
+             ' can be very helpful for troubleshooting problems', default=False
+    )
+    parser.add_argument(
+        '--split_by_phrase', action='store_true',
+        help='When loading the sequences of chords, split by phrases and pad those with deficient length'
+             ' with respect to the maximum length. This allows to randomize partitions. If this parameter'
+             ' is not provided, the sequences will be considered as continuous (resets between change of'
+             ' movements and quartets) and reshaped as if they had the average sequence length.', default=False
+    )
+    parser.add_argument(
+        '--embedding', action='store_false',
+        help='When training the model, do not embed the chords. Instead, use directly the chords.', default=True
+    )
+    parser.add_argument(
+        '--input_file',
+        help='Input file with the first chords and the features. If not provided, a prompt will ask for the desired length'
+             ' and default features will be used.',
+             default='example_input.tsv'
+             )
+    parser.set_defaults(**default_params)
+
+    return parser.parse_args()
